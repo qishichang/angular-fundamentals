f5a57f81e1e98fcc6b038bf22fd5a4a5a94d4f77
qishichang <qishichang@live.com>
2019-06-27 20:33:55 +1200

    Add create event component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event/create-event.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
       EventsAppComponent,
       EventsListComponent,
       EventThumbnailComponent,
-      EventDetailsComponent
+      EventDetailsComponent,
+      CreateEventComponent
    ],
    imports: [
       BrowserModule,
--- src/app/routes.routing.ts
+++ src/app/routes.routing.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event/create-event.component';
 
 const routes: Routes = [
-  { path: 'events', component: EventsListComponent  },
+  { path: 'events/create', component: CreateEventComponent },
+  { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventDetailsComponent },
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: '', redirectTo: '/events', pathMatch: 'full' }
 ];
 
 export const RoutesRoutes = RouterModule.forRoot(routes);
