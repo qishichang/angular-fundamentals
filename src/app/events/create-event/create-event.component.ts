338df396a0e7c85e9b9fc25dfb991dda26b93715
qishichang <qishichang@live.com>
2019-07-08 21:28:06 +1200

    Add initial data for create event form

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -15,6 +15,19 @@ export class CreateEventComponent implements OnInit {
     private eventService: EventService) { }
 
   ngOnInit() {
+    this.newEvent = {
+      name: 'Ng Spectacular',
+      'date': '8/8/2028',
+      time: '10am',
+      price: 799.99,
+      location: {
+        address: '456 Happy St',
+        city: 'Felicity',
+        country: 'Angularistan'
+      },
+      onlineUrl: 'http://ngSpectacular.com',
+      imageUrl: 'http://ngSpectacular.com/logo.png'
+    };
   }
 
   saveEvent(formValues) {
