6ad48b8431e431511d1372f2e1ad381406de90c6
qishichang <qishichang@live.com>
2019-07-17 22:11:35 +1200

    Create settings on simple modal component

--- src/app/shared/components/simple-modal/simple-modal.component.ts
+++ src/app/shared/components/simple-modal/simple-modal.component.ts
@@ -10,6 +10,7 @@ export class SimpleModalComponent implements OnInit {
 
   @Input() title: string;
   @Input() elementId: string;
+  @Input() closeOnBodyClick: string;
   @ViewChild('modalContainer') containerEl: ElementRef;
 
   constructor(@Inject(JQ_TOKEN) private $: any) { }
@@ -18,6 +19,8 @@ export class SimpleModalComponent implements OnInit {
   }
 
   closeModal() {
-    this.$(this.containerEl.nativeElement).modal('hide');
+    if (this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
   }
 }
