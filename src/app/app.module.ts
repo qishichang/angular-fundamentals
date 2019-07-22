3ef69524fbef6e4d4d62f70b2c19c62229d208e5
qishichang <qishichang@live.com>
2019-07-22 21:47:04 +1200

    Prepare proxy server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
@@ -38,6 +39,7 @@ import {
    ],
    imports: [
       BrowserModule,
+      HttpClientModule,
       CoreModule,
       SharedModule,
       EventsRoutingModule
