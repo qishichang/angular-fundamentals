b3400af01772c1d414f6235cf00788d6dbb296ce
qishichang <qishichang@live.com>
2019-07-24 23:22:41 +1200

    Update voter service using HttpClient

--- src/app/events/shared/voter.service.ts
+++ src/app/events/shared/voter.service.ts
@@ -1,22 +1,44 @@
 import { Injectable } from '@angular/core';
-import { ISession } from './event.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { ISession } from './';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  deleteVoter(session: ISession, voterName: string) {
+  deleteVoter(eventId: number, session: ISession, voterName: string) {
     session.voters = session.voters.filter(voter => voter !== voterName);
+    const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+    this.http.delete(url).subscribe();
   }
 
   userHasVoted(session: ISession, voterName: string) {
     return session.voters.some(voter => voter === voterName);
   }
 
-  addVoter(session: ISession, voterName: string) {
+  addVoter(eventId: number, session: ISession, voterName: string) {
     session.voters.push(voterName);
+    const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+    const headers = {
+      'headers': new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.http.post(url, {}, headers)
+      .pipe(catchError(this.handelError('addVoter')))
+      .subscribe();
+
+  }
+
+  private handelError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    };
   }
 }
