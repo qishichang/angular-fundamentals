814b391a3a523d732d2404b975e472e32e759e30
qishichang <qishichang@live.com>
2019-08-10 19:14:41 +1200

    Set up for integrated tests

--- src/app/events/session-list/session-list.component.spec.ts
+++ src/app/events/session-list/session-list.component.spec.ts
@@ -3,15 +3,34 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+
 import { SessionListComponent } from './session-list.component';
+import { AuthService } from 'app/user/shared';
+import { VoterService } from '../shared';
 
 describe('SessionListComponent', () => {
   let component: SessionListComponent;
   let fixture: ComponentFixture<SessionListComponent>;
+  let element: HTMLElement;
+  let debugEl: DebugElement;
 
   beforeEach(async(() => {
+    const mockAuthService = {};
+    const mockVoterService = {};
     TestBed.configureTestingModule({
-      declarations: [ SessionListComponent ]
+      declarations: [ SessionListComponent ],
+      imports: [ FormsModule, RouterTestingModule, HttpClientTestingModule ],
+      providers: [{
+        provide: AuthService,
+        useValue: mockAuthService
+      }, {
+        provide: VoterService,
+        useValue: mockVoterService
+      }],
+      schemas: []
     })
     .compileComponents();
   }));
@@ -19,6 +38,8 @@ describe('SessionListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SessionListComponent);
     component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
