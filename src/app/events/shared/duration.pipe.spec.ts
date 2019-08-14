659123e0106625840ccba0fa91ee850c0b3ff319
qishichang <qishichang@live.com>
2019-08-14 23:51:43 +1200

    Test a pipe

--- src/app/events/shared/duration.pipe.spec.ts
+++ src/app/events/shared/duration.pipe.spec.ts
@@ -3,9 +3,28 @@
 import { TestBed, async } from '@angular/core/testing';
 import { DurationPipe } from './duration.pipe';
 
-describe('Pipe: Duratione', () => {
-  it('create an instance', () => {
-    let pipe = new DurationPipe();
-    expect(pipe).toBeTruthy();
+describe('Pipe: Duration', () => {
+  it('should display Half Hour if duration is 1', () => {
+    const pipe = new DurationPipe();
+
+    expect(pipe.transform(1)).toEqual('Half Hour');
+  });
+
+  it('should display One Hour if duration is 2', () => {
+    const pipe = new DurationPipe();
+
+    expect(pipe.transform(2)).toEqual('One Hour');
+  });
+
+  it('should display Half Day if duration is 3', () => {
+    const pipe = new DurationPipe();
+
+    expect(pipe.transform(3)).toEqual('Half Day');
+  });
+
+  it('should display Full Day if duration is 4', () => {
+    const pipe = new DurationPipe();
+
+    expect(pipe.transform(4)).toEqual('Full Day');
   });
 });
